49e7f904143884fdab4af5026f698b2541cb79b0
dtan4 <dtanshi45@gmail.com>
2013-10-10 12:02:32 +0900

    Refactor class and variables name

--- lib/xygon/cipher.rb
+++ lib/xygon/cipher.rb
@@ -2,17 +2,17 @@ require 'openssl'
 require 'digest'
 
 module Xygon
-  class Crypt
-    def self.decrypt_file(cryptfile, rawfile, pass)
-      once = decrypt_aes256(open(cryptfile).read, pass)
+  class Cipher
+    def self.decrypt_file(encrypted_file, raw_file, pass)
+      once = decrypt_aes256(open(encrypted_file).read, pass)
       decrypted = decrypt_aes256(once, pass.reverse)
-      open(rawfile, "w") { |f| f.puts decrypted }
+      open(raw_file, "w") { |f| f.puts decrypted }
     end
 
-    def self.encrypt_file(rawfile, cryptfile, pass)
-      once = encrypt_aes256(open(rawfile).read, pass)
+    def self.encrypt_file(raw_file, encrypted_file, pass)
+      once = encrypt_aes256(open(raw_file).read, pass)
       encrypted = encrypt_aes256(once, pass.reverse)
-      open(cryptfile, "w") { |f| f.puts encrypted }
+      open(encrypted_file, "w") { |f| f.puts encrypted }
     end
 
     def self.decrypt_aes256(text, pass)
--- spec/xygon/cipher_spec.rb
+++ spec/xygon/cipher_spec.rb
@@ -2,48 +2,48 @@
 require "spec_helper"
 
 module Xygon
-  describe Crypt do
-    let(:rawfile) { File.expand_path("../fixtures/raw", __dir__) }
-    let(:cryptfile) { File.expand_path("../fixtures/crypt", __dir__) }
+  describe Cipher do
+    let(:raw) { File.expand_path("../fixtures/raw", __dir__) }
     let(:encrypted) { File.expand_path("../fixtures/encrypted", __dir__) }
-    let(:decrypted) { File.expand_path("../fixtures/decrypted", __dir__) }
+    let(:_encrypted) { File.expand_path("../fixtures/_encrypted", __dir__) }
+    let(:_decrypted) { File.expand_path("../fixtures/_decrypted", __dir__) }
     let(:pass) { "abcd1234" }
 
     describe "#decrypt_file" do
       before do
-        pending "encrypted file is invalid now"
-        Xygon::Crypt.decrypt_file(cryptfile, decrypted, pass)
+        pending "_encrypted file is invalid now"
+        Xygon::Cipher.decrypt_file(encrypted, _decrypted, pass)
       end
 
       it "should generate decrypted file" do
-        File.exists?(decrypted).should be_true
+        File.exists?(_decrypted).should be_true
       end
 
       it "should return the same to raw file" do
-        open(decrypted).read.should == open(rawfile).read
+        open(_decrypted).read.should == open(raw).read
       end
 
       after do
-        File.delete(decrypted) if File.exists?(decrypted)
+        File.delete(_decrypted) if File.exists?(_decrypted)
       end
     end
 
     describe "#encrypt_file" do
       before do
-        pending "encrypted file is invalid now"
-        Xygon::Crypt.encrypt_file(rawfile, encrypted, pass)
+        # pending "encrypted file is invalid now"
+        Xygon::Cipher.encrypt_file(raw, _encrypted, pass)
       end
 
-      it "should generate encrypted file" do
-        File.exists?(encrypted).should be_true
+      it "should generate _encrypted file" do
+        File.exists?(_encrypted).should be_true
       end
 
       it "should return the same to crypt file" do
-        read_with_ascii(encrypted).should == read_with_ascii(cryptfile)
+        read_with_ascii(_encrypted).should == read_with_ascii(encrypted)
       end
 
       after do
-        File.delete(encrypted) if File.exists?(encrypted)
+        File.delete(_encrypted) if File.exists?(_encrypted)
       end
     end
 
@@ -52,13 +52,13 @@ module Xygon
 
     describe "#decrypt_aes256" do
       it "should decrypt the AES-256 encrypted string" do
-        Xygon::Crypt.decrypt_aes256(encrypted_str, pass).should == raw_str
+        Xygon::Cipher.decrypt_aes256(encrypted_str, pass).should == raw_str
       end
     end
 
     describe "#encrypt_aes256" do
-      it "should encrypt string using AES-256 method" do
-        Xygon::Crypt.encrypt_aes256(raw_str, pass).should == encrypted_str
+      it "should encrypt string" do
+        Xygon::Cipher.encrypt_aes256(raw_str, pass).should == encrypted_str
       end
     end
   end
